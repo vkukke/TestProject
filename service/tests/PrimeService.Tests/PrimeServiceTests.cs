b7893a274a175507ae0099b74c1f6785ce2a3111
Vidya Kukke <vkukke@microsoft.com>
2018-09-14 10:45:35 -0700

    Changed the classname of unitest file

--- service/tests/PrimeService.Tests/PrimeServiceTests.cs
+++ service/tests/PrimeService.Tests/PrimeServiceTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PrimeService.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class PrimeServiceTests
     {
         [TestMethod]
         public void ReturnsFalseWhenValueIs1()
